7a975b2c658c70bdc3209aaddb8d683d21b14834
Ivy-Walobwa <ivywalobwa@gmail.com>
2023-11-13 00:00:02 +0300

    fix: fix wrong date format

--- R/timeline_server.R
+++ R/timeline_server.R
@@ -3,12 +3,12 @@ timelineServer <- function(id, .data) {
     output$plot <- renderPlotly({
       filter_data <- .data()
       # Load the event date
-      date <- as.Date(filter_data$eventDate, "%m/%d/%y")
+      date <- as.Date(filter_data$eventDate, "%Y-%m-%d")
 
       # Display time line with plotly
       p <- ggplot(filter_data, aes(x = date, y = individualCount, size = 3))
       p <- p + labs(title = "Timeline of selected species", x = "Year", y = "Number of occurrences")
-      p <- p + geom_point(alpha = 0.5)
+      p <- p + geom_point(alpha = 0.5, color="#0099F9")
       p <- p + scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month", date_labels = "%Y")
       p <- p + theme_bw()
       p <- p + theme(legend.position = "none")
