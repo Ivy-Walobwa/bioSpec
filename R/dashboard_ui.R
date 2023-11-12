37783064d34b58bd8b76a5b1df12f04b6bafc46f
Ivy-Walobwa <ivywalobwa@gmail.com>
2023-11-12 20:03:27 +0300

    fix: namespace issue

--- R/dashboard_ui.R
+++ R/dashboard_ui.R
@@ -3,8 +3,8 @@ mainContentUI <- function() {
         div(class = "main-content__content py-3 ps-3",
             div(class = "card-body d-flex gap-3",
               div(class = "flex-grow-1 d-flex flex-column gap-3",
-                 mapUI("mapUI"),
-                timelineUI("timelineUI")
+                mapUI("map"),
+                timelineUI("timeline"),
               ),
               searchInputUI("search"),
               hamburgerUI("hamburger")
